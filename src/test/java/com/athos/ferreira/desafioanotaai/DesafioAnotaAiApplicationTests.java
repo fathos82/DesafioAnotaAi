c4153b0fafdbcb0a7c337c4461892cf6a8190b60
Athos Ferreira <fathos82@gmail.com>
2024-02-13 12:42:18 -0300

    feat: Adding the another classes to Structure of project

---
+++ src/test/java/com/athos/ferreira/desafioanotaai/DesafioAnotaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athos.ferreira.desafioanotaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioAnotaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
